74affd04de4d1e1fab572bae98d891ceb5cee57b
etkcad <dario.caric@ericsson.com>
2023-03-13 15:44:40 +0100

    first draft

---
+++ src/store/reducers/result.js
@@ -0,0 +1,18 @@
+import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
+
+const initialState = {
+    results: []
+};
+
+const reducer = ( state = initialState, action ) => {
+    switch ( action.type ) {
+        case actionTypes.STORE_RESULT:
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result})});
+        case actionTypes.DELETE_RESULT:
+            return updateObject(state, {results: state.results.filter(result => result.id !== action.resultElId)});
+    }
+    return state;
+};
+
+export default reducer;
